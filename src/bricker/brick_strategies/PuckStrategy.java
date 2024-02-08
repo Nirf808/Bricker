dee153fb101b607f3abcd54e648df04f3d42d3b4
nirf <nir.finkelstein1@mail.huji.ac.il>
2024-02-08 12:58:33 +0200

    each puck can start at any angle

--- src/bricker/brick_strategies/PuckStrategy.java
+++ src/bricker/brick_strategies/PuckStrategy.java
@@ -4,6 +4,9 @@ import bricker.Constants;
 import bricker.main.BrickerGameManager;
 import danogl.GameObject;
 import danogl.collisions.Layer;
+import danogl.util.Vector2;
+
+import java.util.Random;
 
 public class PuckStrategy implements CollisionStrategy {
     private BrickerGameManager brickerGameManager;
@@ -24,9 +27,17 @@ public class PuckStrategy implements CollisionStrategy {
                             Constants.BALL_SOUND,
                             Constants.BALL_SIZE * 0.75f,
                             thisObj.getCenter(),
-                            brickerGameManager.chooseBallDirection()),
+                            choosePuckDirection()),
                     Layer.DEFAULT);
         }
+    }
 
+    public Vector2 choosePuckDirection(){
+        double speedAbsoluteValue = Math.sqrt(2) * Constants.BALL_SPEED;
+        Random random = new Random();
+        double randomValue = random.nextDouble() * 2 * Math.PI;
+        double xSpeed = speedAbsoluteValue * Math.cos(randomValue);
+        double ySpeed = speedAbsoluteValue * Math.sin(randomValue);
+        return new Vector2((float)xSpeed, (float)ySpeed);
     }
 }
